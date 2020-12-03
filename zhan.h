3264135c60f9d985c36380476729c091b6f7352d
root <root@LAPTOP-AO1QIVKJ.localdomain>
2020-12-03 22:44:55 +0800

    first commit

---
+++ zhan.h
@@ -0,0 +1,43 @@
+#ifndef zhan.h
+
+#define zhan.h
+
+#include<stdio.h>
+
+#include<string.h>
+
+#include<stdlib.h>
+
+
+
+typedef int dataType;
+
+typedef struct node {
+
+			dataType data;
+
+						struct node *next;
+
+}LinkStack,node;
+
+
+
+LinkStack* InitStack();
+
+int Empty(LinkStack *s);
+
+void Push(LinkStack *s,dataType x);
+
+void Pop(LinkStack *s);
+
+dataType GetTop(LinkStack *s);
+
+int priority(char x);
+
+double compute(double x, double y,char op);
+
+void linkstack();
+
+
+
+#endif
