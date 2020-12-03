9fbd97f8039e47cac4fd7fff70d9299805ed0f4b
root <root@LAPTOP-AO1QIVKJ.localdomain>
2020-12-03 23:25:14 +0800

    first commit

---
+++ zhan4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+#include<string.h>
+
+#include"zhan4.c"
+
+
+
+int main(){
+
+	LinkStack *s=InitStack();
+
+	Push(s,80);
+
+	Push(s,90);
+
+	Pop(s);
+
+	Push(s,70);
+
+	printf("实验的数值为:%d",GetTop(s));
+
+	printf("\n");
+
+	linkstack();
+
+}
